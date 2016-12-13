54718fd5cd173f8d7aeda8aee27e02cb04d50fb8
brandon <mn.cres@gmail.com>
2016-12-12 19:56:17 -0600

    add wireframe section

--- src/Home.vue
+++ src/Home.vue
@@ -1,20 +1,27 @@
 <template>
-    <div class="main">
-        <app-nav></app-nav>
+    <div>
+        <div class="main">
+            <app-nav></app-nav>
 
-        <div class="info">
-            <h1>Brandon Gladen</h1>
-            <h3><em>Web Developer</em></h3>
-            <p>(218) 556-4588</p>
-            <p>me@brandongladen.com</p>
-            <div>
-                <a href="http://github.com/bg87"><img src="./assets/github.png" alt="github" class="social" /></a>
-                <a href="https://www.linkedin.com/in/brandon-gladen-19a452109"><img src="./assets/linkedin.png" alt="linkdedin" class="social" /></a>
+            <div class="info">
+                <h1>Brandon Gladen</h1>
+                <h3><em>Web Developer</em></h3>
+                <p>(218) 556-4588</p>
+                <p>me@brandongladen.com</p>
+                <div>
+                    <a href="http://github.com/bg87"><img src="./assets/github.png" alt="github" class="social" /></a>
+                    <a href="https://www.linkedin.com/in/brandon-gladen-19a452109"><img src="./assets/linkedin.png" alt="linkdedin" class="social" /></a>
+                </div>
+            </div>
+
+            <img  src="./assets/icarus.png" alt="icarus" class="icarus"/>
+
+        </div>
+        <div class="wireframe">
+            <div class="about">
+                <h1>I make websites and applications using a variety of languages and frameworks.</h1>
             </div>
         </div>
-
-        <img  src="./assets/icarus.png" alt="icarus" class="icarus"/>
-
     </div>
 </template>
 
@@ -50,5 +57,39 @@
         float: right;
         margin-right: 2em;
         margin-top: 8em;
+    }
+    .wireframe {
+        width: 100%;
+        height:42em;
+    
+        background: url('assets/wireframe.png') no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+    .about {
+        padding: 8em;
+        text-align: center;
+        color: white;
+    }
+
+    @media (max-width: 400px) {
+        .wireframe {
+            width: 100%;
+            height:31em;
+        
+            background: url('assets/wireframe.png') no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        .about {
+            padding: 8em;
+            text-align: center;
+            color: white;
+            font-size: .7em;
+        }
     }
 </style>
