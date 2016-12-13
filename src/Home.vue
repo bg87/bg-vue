30dce37cbfa661b1d0b57396b4e59ade658947c1
brandon <mn.cres@gmail.com>
2016-12-12 19:16:30 -0600

    icarus

--- src/Home.vue
+++ src/Home.vue
@@ -8,11 +8,12 @@
             <p>(218) 556-4588</p>
             <p>me@brandongladen.com</p>
             <div>
-                <a href="http://github.com/bg87"><img src="./assets/github.png" alt="github" /></a>
-                <a href="https://www.linkedin.com/in/brandon-gladen-19a452109"><img src="./assets/linkedin.png" alt="linkdedin" /></a>
+                <a href="http://github.com/bg87"><img src="./assets/github.png" alt="github" class="social" /></a>
+                <a href="https://www.linkedin.com/in/brandon-gladen-19a452109"><img src="./assets/linkedin.png" alt="linkdedin" class="social" /></a>
             </div>
         </div>
 
+        <img  src="./assets/icarus.png" alt="icarus" class="icarus"/>
 
     </div>
 </template>
@@ -25,20 +26,29 @@
     .main {
         background: linear-gradient(to right bottom, white 50%, #d5e0f6 50%);
         height: 100vh;
+        position: relative;
     }
     .info {
         text-align: center;
         margin-top: 30px;
     }
-    img:hover {
+    .social:hover {
        -webkit-transform: rotateZ(15deg); /* Safari */
         transform: rotateZ(15deg);
     }
 
-    img {
+    .social {
         height: 2.5em;
         width: 2.5em;
         transition: .3s;
         opacity: 0.85;
+    }
+    .icarus {
+        opacity: 0.04;
+        height: 11em;
+        width:12em;
+        float: right;
+        margin-right: 2em;
+        margin-top: 8em;
     }
 </style>
